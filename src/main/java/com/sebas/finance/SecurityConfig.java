5b0b9c6a2b5fca7c2e3def2293e72faa40f20f89
urfavsebxs <devsebitas@gmail.com>
2026-05-28 14:47:54 -0500

    feat: eliminar acceso público a la ruta de usuarios en la configuración de seguridad

--- src/main/java/com/sebas/finance/SecurityConfig.java
+++ src/main/java/com/sebas/finance/SecurityConfig.java
@@ -34,7 +34,6 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/v1/auth/**").permitAll()
-                        .requestMatchers("/api/v1/users/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
