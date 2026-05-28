0825f8501cc4ce46d0ee496170c24fae107c820e
urfavsebxs <devsebitas@gmail.com>
2026-05-28 12:46:00 -0500

    feat: implementar modelo y repositorio de usuario con validaciones

---
+++ src/main/java/com/sebas/finance/module/user/domain/model/UserFullName.java
@@ -0,0 +1,11 @@
+package com.sebas.finance.module.user.domain.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@RequiredArgsConstructor
+@Getter
+public class UserFullName {
+    private final String value;
+}
