48f452611535f172c88a7628511e9cb3185fa6db
urfavsebxs <devsebitas@gmail.com>
2026-05-28 15:11:23 -0500

    feat: add Dockerfile and docker-compose configuration for backend services feat: update CORS configuration to allow requests from the production URL

--- src/main/java/com/sebas/finance/SecurityConfig.java
+++ src/main/java/com/sebas/finance/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4321"));
+        configuration.setAllowedOrigins(List.of("https://finance-studio-mu.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
